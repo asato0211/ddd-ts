a556967a101f3841d76fcbc1db221945bc453c5a
asato <mai0926yhm@gmail.com>
2024-02-08 22:20:04 +0900

    リポジトリを実装

--- api/src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
+++ api/src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
@@ -1,9 +1,12 @@
 import { BookId } from 'Domain/models/Book/BookId/BookId';
+import { IBookRepository } from 'Domain/models/Book/IBookRepository';
 
 export class ISBNDuplicationCheckDomainService {
+  constructor(private bookRepository: IBookRepository) {}
+
   async execute(isbn: BookId): Promise<boolean> {
-    // 本来は、データベースに問い合わせて重複があるか確認する。この章では省略。
-    const isDuplicateISBN = false;
+    const duplicateISBNBook = await this.bookRepository.find(isbn);
+    const isDuplicateISBN = !!duplicateISBNBook;
 
     return isDuplicateISBN;
   }
